c4315cda33e0653212a15cf1777b6e0d6aa6e996
dmytrenkojulia <dmytrenkojulia@gmail.com>
2021-01-26 03:56:14 +0100

    feat: dodano miniaturki ratownikow

--- application/views/components/ratownik_tile.component.php
+++ application/views/components/ratownik_tile.component.php
@@ -1,9 +1,14 @@
 <?php
 function createRatownikTile($post) {
 
-	echo '
-		<div class="card mb-3" >
-			<p class="card-text">'.$post['imie'].'</p>
+	echo '		
+		<div class="card" ">
+		  <img class="card-img-top" src="https://static.wirtualnemedia.pl/media/top/blowek655.png" alt="Zdjęcie ratownika">
+		  <div class="card-body">
+			<h5 class="card-title">'.$post['imie'].' '.$post['nazwisko'].'</h5>
+			<p class="card-text">jakies info</p>
+			<a href="#" class="btn btn-primary btn-block">Dodaj do jednostki</a>
+		  </div>
 		</div>
 	';
 }
--- application/views/ratownicy/index.php
+++ application/views/ratownicy/index.php
@@ -1,12 +1,17 @@
-<h2>Ratownicy</h2>
+<div class="container">
+	<div class="my-5 d-flex flex-wrap">
+		<?php
+		include "./application/views/components/ratownik_tile.component.php";
+		//			echo json_encode($posts);
 
-<div class="my-4">
-	<?php
-	include "./application/views/components/ratownik_tile.component.php";
-	//			echo json_encode($posts);
-	foreach ($ratownicy as $ratownik) :
-		createRatownikTile($ratownik);
-	endforeach;
-	?>
+		foreach ($ratownicy as $ratownik) : ?>
+			<div class="col mb-3" style="flex: 0 1 25%; min-width: 15rem;">
+				<?php createRatownikTile($ratownik); ?>
+			</div>
+		<?php
+		endforeach;
+		?>
+	</div>
 </div>
+
 
