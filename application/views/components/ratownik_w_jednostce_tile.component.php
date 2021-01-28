f728e8520b7a0dcd4efa004a67bb8679e814a96b
dmytrenkojulia <dmytrenkojulia@gmail.com>
2021-01-28 15:15:42 +0100

    refactor: jednak redirect byl dobrym rozwiazaniem xd

--- application/views/components/ratownik_w_jednostce_tile.component.php
+++ application/views/components/ratownik_w_jednostce_tile.component.php
@@ -1,8 +1,6 @@
 <?php
 function createRatownikWJednostceTile($ratownik) {
 
-	$usunURL = '\''. site_url('jednostka/usun/'.$ratownik['id']) .'\'';
-
 	echo '		
 		<div class="card">
 		  <img class="card-img-top" src="https://static.wirtualnemedia.pl/media/top/blowek655.png" alt="Zdjęcie ratownika">
@@ -10,7 +8,7 @@ function createRatownikWJednostceTile($ratownik) {
 			<h5 class="card-title">'.$ratownik['imie'].' '.$ratownik['nazwisko'].'</h5>
 			<p class="card-text">jakies info</p>
 			<a href="'.site_url('jednostka/edytuj/'.$ratownik['id']).'" class="btn btn-primary">Edytuj</a>
-			<button onclick="usunRatownikaZJednostki('.$usunURL.')" class="btn btn-danger float-right">Usuń</button>
+			<a href="'.site_url('jednostka/usun/'.$ratownik['id']).'" class="btn btn-danger float-right">Usuń</a>
 		  </div>
 		</div>
 	';
