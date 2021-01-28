530fbd2045f844caef77b86aed87eb5bf3a56604
dmytrenkojulia <dmytrenkojulia@gmail.com>
2021-01-28 18:40:14 +0100

    fix: sortuj ratownikow po nr id

--- application/models/Ratownik_model.php
+++ application/models/Ratownik_model.php
@@ -2,6 +2,7 @@
 class Ratownik_model extends CI_Model {
 
 	public function get_ratownicy() {
+		$this->db->order_by('id', 'ASC');
 		$query = $this->db->get_where('pracownicy', array('stanowisko' => 'ratownik'));
 		return $query->result_array();
 	}
